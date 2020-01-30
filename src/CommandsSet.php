9c95e77e0f2b8c970f1160310b8ab750f1b97295
Vlad Reshetilo <vlad.reshetilo@lamp-dev.com>
2020-01-30 14:57:57 +0200

    refactoring and support of boolean options

---
+++ src/CommandsSet.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace VladReshet\ArtisanUI;
+
+// DTO for menu settings and commands
+class CommandsSet
+{
+    private $commands = [
+        'all' => [],
+        'favourite' => [],
+        'grouped' => []
+    ];
+
+    public function setAll(array $commands) :self
+    {
+        $this->commands['all'] = $commands;
+
+        return $this;
+    }
+
+    public function setFavourite(array $commands) :self
+    {
+        $this->commands['favourite'] = $commands;
+
+        return $this;
+    }
+
+    public function setGrouped(array $commands) :self
+    {
+        $this->commands['grouped'] = $commands;
+
+        return $this;
+    }
+
+    public function getAll() :array
+    {
+        return $this->commands['all'];
+    }
+
+    public function getFavourite() :array
+    {
+        return $this->commands['favourite'];
+    }
+
+    public function getGrouped() :array
+    {
+        return $this->commands['grouped'];
+    }
+
+    public function hasFavourite() :bool
+    {
+        return !empty($this->commands['favourite']);
+    }
+
+    public function isFavourite(string $name) :bool
+    {
+        return in_array($name, $this->commands['favourite']);
+    }
+}
